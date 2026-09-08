383810ade1931227b5ae76acff6e42c75c652880
Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>
2026-09-08 20:28:44 +0530

    Restore files lost in accidental mass-deletion merge (PR #21)

---
+++ authentication-demo-ui/src/main/java/io/mosip/authentication/demo/dto/EncryptionResponseDto.java
@@ -0,0 +1,16 @@
+package io.mosip.authentication.demo.dto;
+
+import lombok.Data;
+
+/**
+ * The Class EncryptionResponseDto.
+ * 
+ * @author Sanjay Murali
+ */
+@Data
+public class EncryptionResponseDto {
+	String encryptedSessionKey;
+	String encryptedIdentity;
+	String requestHMAC;
+	String thumbprint;
+}
